e09ced2eec072f5bc9b3a43a952d65352657471a
smiath <smiath.yt@gmail.com>
2025-10-30 15:25:11 +0100

    added bonus compute

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from typing import List, Dict, Callable
+from typing import List, Dict, Callable, Tuple, TypeAlias
 from figures import isYAMS, isSuite, isCarre, isFull, isBrelan
 
+Roll: TypeAlias = List[int]
+FigureCheck: TypeAlias = Callable[[Roll], bool]
+FigureScore: TypeAlias = int
+FigureMap: TypeAlias = Dict[FigureCheck, FigureScore]
+CalculationResult: TypeAlias = Tuple[FigureScore, FigureCheck | None]
 
-ORDERED_POSSIBLE_FIGURES: Dict[Callable[[List[int]], bool], int] = {
+ORDERED_POSSIBLE_FIGURES: FigureMap = {
     isYAMS: 50,
     isSuite: 40,
     isCarre: 35,
@@ -11,26 +16,35 @@ ORDERED_POSSIBLE_FIGURES: Dict[Callable[[List[int]], bool], int] = {
 }
 
 
-def checkNumberOfDices(dices: List[int]) -> None:
+def checkNumberOfDices(dices: Roll) -> None:
     if len(dices) != 5:
         raise ValueError("Number of dices must be 5")
 
 
-def calculateOneRoll(dices: List[int]) -> int:
+def calculateOneRoll(dices: Roll, available_figures: FigureMap = ORDERED_POSSIBLE_FIGURES) -> CalculationResult:
     checkNumberOfDices(dices)
 
-    for function, value in ORDERED_POSSIBLE_FIGURES.items():
+    for function, value in available_figures.items():
         if function(dices):
-            return value
+            return value, function
 
-    return sum(dices)
+    return sum(dices), None
 
 
-def main(rolls: List[List[int]]) -> int:
+def main(rolls: List[Roll]) -> int:
 
     resultat: int = 0
+    
+    available_figures: FigureMap = ORDERED_POSSIBLE_FIGURES.copy()
 
     for roll in rolls:
-        resultat += calculateOneRoll(roll)
+        score: FigureScore
+        function: FigureCheck | None
+        score, function = calculateOneRoll(roll, available_figures)
+        
+        if function is not None:
+            del available_figures[function]
+            
+        resultat += score
 
     return resultat
